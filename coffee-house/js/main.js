073133995f7d3e2b97db3cf031cd98bf586bef2b
grimpatron <Hellhammer220bpm@gmail.com>
2023-12-04 10:27:15 +0300

    feat: make adaptation for mobile (Mon, Dec 04, 2023 10:28:00 AM)

--- coffee-house/js/main.js
+++ coffee-house/js/main.js
@@ -1,7 +1,19 @@
 (function () {
-  var toggle = document.querySelector('.header__burger-button');
-  
+  let toggle = document.querySelector('.header__burger-button');
+  let burgerOverlay = document.querySelector('.header__nav');
+
   toggle.addEventListener('click', function(e) {
     this.classList.toggle('opened');
+    burgerOverlay.classList.toggle('header__nav--active');
   });
 })();
+
+
+let menuCards = document.querySelectorAll('.coffee-on-offer .on-offer__card');
+console.log(menuCards);
+
+function hideCards(menuCards) {
+  for (let index = 0; index < menuCards.length; index++) {
+    if (index >= 4) menuCards[index].style.display = 'none';
+  }
+} hideCards(menuCards);
