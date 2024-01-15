fa42cde85472dc32b973bc33796124edc0e6d062
Mikhail Khilman <Hellhammer220bpm@gmail.com>
2024-01-16 00:07:56 +0300

    feat: finish task

--- hangman/main.js
+++ hangman/main.js
@@ -1,17 +1,44 @@
 let attemptsNumber = 0;
 let failCounter = 0;
 
-const mainHTML = document.querySelector('.main');
 const keys = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const hmArr = ['hm-1', 'hm-2', 'hm-3', 'hm-4', 'hm-5', 'hm-6', 'man-head', 'man-body', 'man-arm-l', 'man-arm-r', 'man-leg-l', 'man-leg-r']
+let thisWord = '';
+let pressedKeys = [];
 
-
-
+/* Generate HTML */
+const bodyHTML = document.querySelector('body');
+bodyHTML.innerHTML = `<div class="container"><div class="main"></div></div>`;
+const mainHTML = document.querySelector('.main');
+generateHangman();
 function generateInput() {
-  mainHTML.innerHTML += '<div id="termLine"></div>';
-  mainHTML.innerHTML += '<div id="clueLine"></div>';
-  mainHTML.innerHTML += '<div id="attemptsNumber">0</div>';
+  mainHTML.innerHTML += '<div id="termLine"></div><div id="clueLine"></div>';
   mainHTML.innerHTML += '<div class="guesses">Incorrect guesses: <span id="failNumber">0</span> / <span>6</span></div>';
-  // mainHTML.innerHTML += '<input type="text" id="input">';
+  mainHTML.innerHTML += `
+  <div id="myModal" class="modal">
+    <div class="modal-content">
+      <div id="modalMessage"></div>
+      <div>Correct answer: <span id="modalAnswer"></span></div>
+      <button id="modalBtn">Once again</button>
+    </div>
+  </div>`
+}
+generateInput();
+generateKeyboard();
+
+
+
+function generateHangman() {
+  const hangman = document.createElement('div');
+  hangman.classList.add('hangman');
+
+  for (let i = 0; i < 13; i++) {
+    const key = document.createElement('div');
+    key.classList.add('hm');
+    key.classList.add(hmArr[i]);
+    hangman.appendChild(key);
+  }
+  mainHTML.appendChild(hangman);
 }
 
 function generateKeyboard() {
@@ -27,8 +54,6 @@ function generateKeyboard() {
   mainHTML.appendChild(keyboard);
 }
 
-generateInput();
-generateKeyboard();
 
 
 const keyList = document.querySelectorAll('.key');
@@ -38,35 +63,35 @@ keyList.forEach(el => el.addEventListener('click', function (e) {
 }));
 
 document.addEventListener('keydown', function(event) {
-  checkChar((event.key).toUpperCase());
+  if (keys.includes((event.key).toUpperCase())) {
+    checkChar((event.key).toUpperCase());
+  }
 });
 
 
 
 function checkChar(char) {
-  let flag = false;
-  // let attemptsNumberVar = document.getElementById('attemptsNumber');
-  // let attemptsNumberVarCopy = attemptsNumberVar.innerHTML;
-  for (let i = 0; i < termAnswer.length; i++) {
-    if (termAnswer[i] == char) {
-      termCurrent[i] = char;
-      mask[i] = true;
-      // attemptsNumberVar.innerHTML = ++attemptsNumber;
-      flag = true;
+  console.log(pressedKeys, char);
+  if(pressedKeys.includes(char) == false) {
+    pressedKeys.push(char);
+    let flag = false;
+    for (let i = 0; i < termAnswer.length; i++) {
+      if (termAnswer[i] == char) {
+        termCurrent[i] = char;
+        mask[i] = true;
+        flag = true;
+      }
     }
-  }
-  
-  if (flag == false) {
-    document.getElementById('failNumber').innerHTML = ++failCounter;
-  }
+    
+    if (flag == false) {
+      document.getElementById('failNumber').innerHTML = ++failCounter;
+    }
 
-  // if (attemptsNumber == attemptsNumberVarCopy) {
-  //   document.getElementById('failNumber').innerHTML = ++failCounter;
-  // }
-  blockingKey(char);
-  updateWord();
-  gameStatus();
-  drawMan();
+    blockingKey(char);
+    updateWord();
+    gameStatus();
+    drawMan();
+  }
 }
 
 
@@ -77,7 +102,6 @@ function blockingKey(char) {
     }
   });
 }
-
 
 
 
@@ -102,20 +126,23 @@ let termAnswer = [];
 let termCurrent = [];
 function generateWord() {
   let word = wordList[Math.floor(Math.random() * wordList.length)];
+  console.log(thisWord, word[0]);
+  if (thisWord == word[0]) {
+    generateWord();
+  } 
+  mask = [];
   termAnswer = [];
   termCurrent = [];
   term = word[0];
-  let clue = word[1];
-  console.log(term, clue);
   for (let index = 0; index < term.length; index++) {
     mask.push(false);
     termAnswer.push(term[index].toUpperCase());
     termCurrent.push('_');
-    // termLine.innerHTML += `<span class='term__letter'>${term[index]}<span>`;
   }
+  thisWord = word[0];
   updateWord();
-  clueLine.innerHTML = clue;
-  console.log(mask, termAnswer, termCurrent);
+  clueLine.innerHTML = word[1];
+  // console.log(mask, termAnswer, termCurrent);
 }
 generateWord();
 
@@ -130,18 +157,17 @@ function updateWord() {
 
 function gameStatus() {
   const xxx = mask.every(user => user == true);
-  if (xxx == true) alert("Yes!!");
-  if (failCounter >= 6 ) showModal();
+  if (xxx == true) showModal(true);
+  if (failCounter >= 6 ) showModal(false);
 }
 
 
 const manHead = document.querySelector('.man-head');
 const manBody = document.querySelector('.man-body');
-const manArmLeft = document.querySelector('.man-arm-left');
-const manArmRight = document.querySelector('.man-arm-right');
-const manLegLeft = document.querySelector('.man-leg-left');
-const manLegRight = document.querySelector('.man-leg-right');
-const modalAnswer = document.querySelector('#modalAnswer');
+const manArmLeft = document.querySelector('.man-arm-l');
+const manArmRight = document.querySelector('.man-arm-r');
+const manLegLeft = document.querySelector('.man-leg-l');
+const manLegRight = document.querySelector('.man-leg-r');
 
 function drawMan(){
   switch (failCounter) {
@@ -158,30 +184,32 @@ function drawMan(){
 
 
 
-let modal = document.getElementById("myModal");
-let span = document.getElementsByClassName("close")[0];
-function showModal() {
+const messageArr = ["Well done! You were able to guess the word!", "You couldn't guess the word!"];
+const modalAnswer = document.querySelector('#modalAnswer');
+const modalMessage = document.querySelector('#modalMessage');
+const modal = document.getElementById("myModal");
+function showModal(status) {
   modal.style.display = "block";
   modalAnswer.innerHTML = term;
-}
-span.onclick = function() {
-  modal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+  if (status == true) modalMessage.innerHTML = messageArr[0];
+  if (status == false) modalMessage.innerHTML = messageArr[1];
 }
 modalBtn.onclick = function() {
   modal.style.display = "none";
-  failCounter = 0;
   generateWord();
   clear();
 }
 
 function clear() {
   const showEl = document.querySelectorAll('.show');
+  const keyContain = document.querySelectorAll('.contain');
   showEl.forEach(element => {
     element.classList.remove('show');
   });
+  keyContain.forEach(element => {
+    element.classList.remove('contain');
+  });
+  pressedKeys = [];
+  failCounter = 0;
+  document.getElementById('failNumber').innerHTML = 0;
 }
