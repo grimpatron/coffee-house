6f6d314c4d510b8c389caed3a8b8888a92ca09bf
grimpatron <Hellhammer220bpm@gmail.com>
2024-01-31 13:24:21 +0300

    geat: make the first prototype

---
+++ nonograms/main.js
@@ -0,0 +1,98 @@
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach(el => el.addEventListener('click', (e) => {
+  e.target.classList.toggle('cell--black');
+}));
+
+
+const smile = [
+  ['1', '1', '0', '1', '1'], 
+  ['1', '1', '0', '1', '1'], 
+  ['0', '0', '0', '0', '0'], 
+  ['1', '0', '0', '0', '1'], 
+  ['0', '1', '1', '1', '0']
+]
+
+
+
+
+function countRowCells(matrixXXX) {
+  let colCounts = matrixXXX[0].map((col, i) => {
+    let counts = [];
+    let count = 0;
+    for (let row of matrixXXX) {
+      if (row[i] === '1') {
+        count++;
+      } else if (count > 0) {
+        counts.push(count);
+        count = 0;
+      }
+    }
+    if (count > 0) counts.push(count);
+    return counts;
+  });
+
+  return colCounts;
+}
+let resCol = countRowCells(smile);
+
+
+
+function countColCells(matrixXXX) {
+  let rowCounts = matrixXXX.map(row => {
+    let counts = [];
+    let count = 0;
+    for (let cell of row) {
+      if (cell === '1') {
+        count++;
+      } else if (count > 0) {
+        counts.push(count);
+        count = 0;
+      }
+    }
+    if (count > 0) counts.push(count);
+    return counts;
+  });
+
+  return rowCounts;
+}
+let resRow = countColCells(smile);
+
+
+
+// заполняем пустыми строками отсутсвующие ячейки в подсказках.
+function addEmptyValues(arr) {
+  return arr.map(subArr => {
+    while (subArr.length < 3) { subArr.unshift('') }
+    return subArr.slice(0, 3);
+  });
+}
+resCol = addEmptyValues(resCol);
+resRow = addEmptyValues(resRow);
+
+
+
+// переворачивает матрицу на 90 против часовой
+function rotateMatrix(matrix) {
+  let rotatedMatrix = [];
+  for(let i = 0; i < matrix[0].length; i++) {
+    rotatedMatrix[i] = [];
+    for(let j = 0; j < matrix.length; j++) {
+      rotatedMatrix[i][j] = matrix[matrix.length - j - 1][i];
+    }
+  }
+  return rotatedMatrix;
+}
+let rotatedMatrix = rotateMatrix(resCol);
+let flattenedMtrxCol = rotatedMatrix.flat();
+let flattenedMtrxRow = resRow.flat();
+
+
+const clueRowMatrix = document.querySelectorAll('.clue-row .cell');
+const clueColMatrix = document.querySelectorAll('.clue-col .cell');
+fillInClue(clueRowMatrix, flattenedMtrxRow);
+fillInClue(clueColMatrix, flattenedMtrxCol);
+
+function fillInClue(clueElArr, valueArr) {
+  clueElArr.forEach((el, i) => el.innerHTML = valueArr[i]);
+}
