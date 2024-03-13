512b70bfa8bd82d174c617ef582e30520271211d
grimpatron <Hellhammer220bpm@gmail.com>
2024-03-13 13:47:45 +0300

    init: make project

--- src/app/components/text-skeleton/text-skeleton.ts
+++
@@ -1,5 +0,0 @@
-import { div } from '@components/tags';
-
-import styles from './text-skeleton.module.scss';
-
-export const TextSkeleton = () => div({ className: `${styles.skeleton} ${styles.skeletonText}` });
--- src/app/utils/wait.ts
+++
@@ -1,5 +0,0 @@
-export function wait(tShirtSize: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, tShirtSize);
-  });
-}
