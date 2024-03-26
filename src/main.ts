42720f52a9722d1ef7e89f4808aa02b0cb8c4d8f
grimpatron <Hellhammer220bpm@gmail.com>
2024-03-26 18:53:28 +0300

    feat: make basic structure (garage and winners)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,61 @@
+/* eslint-disable */
 import './styles/normalize.css';
 import './styles/my-reset.css';
 import './styles/style.css';
 
-import createDiv from './nodeGenerator.ts';
+import {createDiv} from './nodeGenerator.ts';
+import {createButton} from './nodeGenerator.ts';
 
-const newNode = createDiv('app', 'app');
-const parentElement = document.querySelector<HTMLDivElement>('body');
-if (parentElement) {
-  parentElement.insertBefore(newNode, parentElement.firstChild);
+const newNode = createDiv('header', 'header');
+const bodyElement = document.querySelector<HTMLDivElement>('body');
+if (bodyElement) {
+  bodyElement.insertBefore(newNode, bodyElement.firstChild);
 }
+
+const appDiv = document.getElementById('header')!;
+const topBarDiv = createDiv('topbar');
+const topBarInterfaceDiv = createDiv('topbar-interface');
+const topBarPageDiv = createDiv('topbar-page');
+const pageButtonsDiv = createDiv('page-buttons');
+const garageBtn = createButton('Home', 'btn', 'garageBtn');
+const scoreBtn = createButton('About', 'btn', 'scoreBtn');
+
+pageButtonsDiv.appendChild(garageBtn);
+pageButtonsDiv.appendChild(scoreBtn);
+topBarInterfaceDiv.appendChild(pageButtonsDiv);
+topBarDiv.append(topBarInterfaceDiv, topBarPageDiv);
+appDiv.appendChild(topBarDiv);
+
+
+interface PageConfig {
+  title: string;
+  background: string;
+}
+
+const pages: Record<string, PageConfig> = {
+  garage: {
+    title: "Garage",
+    background: "garage.jpg",
+  },
+  winners: {
+    title: "Winners",
+    background: "blacklist.jpg",
+  },
+};
+
+function showPage(pageName: keyof typeof pages): void {
+  const app = document.querySelector(".topbar-page") as HTMLElement;
+  const pageContent: string = `<h1 class="page-title">${pages[pageName].title} (<span>${0}</span>)</h1>
+  <h2 class="page-subtitle">Page #<span>${0}</span></h2>`;
+  bodyElement!.style.backgroundImage = `url(img/${pages[pageName].background})`;
+  app.innerHTML = pageContent;
+}
+
+document
+  .getElementById("garageBtn")!
+  .addEventListener("click", () => showPage("garage"));
+document
+  .getElementById("scoreBtn")!
+  .addEventListener("click", () => showPage("winners"));
+
+showPage("garage");
--- src/nodeGenerator.ts
+++ src/nodeGenerator.ts
@@ -1,6 +1,14 @@
-export default function createDiv(className: string, id: string): HTMLDivElement {
+export function createDiv(className: string, id?: string): HTMLDivElement {
   const div = document.createElement('div');
   div.className = className;
-  div.id = id;
+  if (id) div.id = id;
   return div;
+}
+
+export function createButton(text: string, className: string, id: string): HTMLButtonElement {
+  const button = document.createElement('button');
+  button.className = className;
+  button.id = id;
+  button.textContent = text;
+  return button;
 }
