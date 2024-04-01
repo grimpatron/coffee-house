705c41ceca85ba0336caaf296c18b050bb9a020e
grimpatron <hellhammer220bpm@gmail.com>
2024-04-02 00:57:19 +0300

    fix: another dev build fix

--- src/app/storage/local-storage.ts
+++
@@ -1,6 +0,0 @@
-export function getStorageData() {
-  const localStorageData= localStorage.getItem('puzzle-user-data');
-  if (localStorageData!== null) {
-    return JSON.parse(localStorageData);
-  }
-}
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import './styles/my-reset.css';
-import './styles/main-screen.css';
-import './styles/style.css';
-
-import './app/components/form/form-validation.ts';
-import './app/components/form/form-storage.ts';
-import './app/interaction.ts';
