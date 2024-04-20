f0847f059101def0000735116d8f68afb60f2653
grimpatron <hellhammer220bpm@gmail.com>
2024-04-20 20:08:55 +0300

    feat: make chat layout

--- fun-chat/src/main.ts
+++ fun-chat/src/main.ts
@@ -1,7 +1,9 @@
-import './my-reset.css'
-import './style.css'
-import {generateLoginForm} from './templates.ts'
-import {validateForm} from './validation.ts'
+import './my-reset.css';
+import './style.css';
+import './chat.css';
+import {generateLoginForm, generateChatLayout} from './templates.ts';
+import {validateForm} from './validation.ts';
 
 (document.querySelector('body') as HTMLElement).innerHTML = generateLoginForm();
 (document.querySelector('#authorization-submit') as HTMLElement).addEventListener('click', validateForm);
+(document.querySelector('body') as HTMLElement).innerHTML = generateChatLayout();
--- fun-chat/src/templates.ts
+++ fun-chat/src/templates.ts
@@ -12,3 +12,81 @@ export function generateLoginForm() {
 </div>
   `;
 }
+
+
+export function generateChatLayout() {
+  return `
+  <div class="container">
+
+  <header class="header">
+    <h1 class="header_title">Fun Chat</h1>
+    <div class="header_user">Grim Patron</div>
+    <button class="header_logout btn">Log Out</button>
+  </header>
+
+  <main class="main">
+
+    <div class="conversation_topbar">
+      <div class="conversation_user">Джу</div>
+      <div class="conversation_status">В сети</div>
+    </div>
+
+    <ul class="conversation">
+
+    </ul>
+
+    <div class="conversation_downbar">
+      <textarea class="textarea" placeholder="Write a message" rows="3"></textarea>
+      <button class="textarea-btn btn">Send</button>
+    </div>
+  </main>
+
+  <footer class="footer">
+    <div class="footer_course">
+      <a href="https://rs.school/">
+        <img src="./public/rss.svg" alt="RS School">
+      </a>
+    </div>
+    <div class="footer_author">
+      <a href="https://github.com/grimpatron">Grim Patron</a>
+    </div>
+    <div class="footer_year">2024</div>
+  </footer>
+
+  <aside class="aside">
+    <input class="users-search" type="text" name="" id="" placeholder="Search">
+    <ul class="users_list">
+    </ul>
+  </aside>
+      
+</div>
+  `;
+}
+
+
+export function generateChatMessage(author: string, time: string, message: string) {
+  return `
+    <li class="message">
+      <div class="message_info">
+        <span class="message_author">${author}</span>
+        <span class="message_time">${time}</span>
+      </div>
+      <div class="message_text">${message}</div>
+    </li>
+  `;
+}
+
+
+export function generateUser(user: string, status: string) {
+  let elemOffline = ''
+  let statusOffline = ''
+  if (status === 'offline') elemOffline = 'users_item--offline';
+  if (status === 'offline') statusOffline = 'users_item-status--offline';
+
+  return `
+    <li class="users_item ${elemOffline}">
+      <div class="users_item-name">${user}</div>
+      <div class="users_item-status ${statusOffline}">${status}</div>
+    </li>
+  `;
+}
